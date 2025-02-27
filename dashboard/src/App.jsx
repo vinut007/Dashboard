c3b8124a150d07fa92ba1847379c17edc26e24d7
vinut007 <vinutnaik60@gmail.com>
2025-02-27 10:25:12 +0530

    first commit

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Main1 from './Components/main1/main1'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Main1/>
+    
+      
+    </>
+  )
+}
+
+export default App
---
+++ dashboard/src/Components/main1/main1.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import './main1.css';
+import payment from './OIP.jpg';
+
+
+
+const Main1 = () => {
+    return (
+        <>
+        <div className="main">
+            <div className="main1">
+                <div className="main1-sub0">
+                    
+                <span className="nextpayment" > <img src={payment} alt="" className="payment"/></span>
+                  <span className="nextpayment">Next Payment </span>  
+                     </div>
+                <div className="main1-sub1"> <span className="main1-sub1-span"></span> <span className="main1-sub1-span"></span> </div>
+                <div className="main1-sub1"> </div>
+                <div className="main1-sub1"> </div>
+                <div className="main1-sub1"> </div>
+                <div className="main1-sub1"> </div>
+                
+
+
+
+
+
+
+            </div>
+            <div className="main2">
+
+
+
+            </div>
+
+
+
+
+
+
+
+        </div>
+
+
+            
+        </>
+    )
+}
+export default Main1;
